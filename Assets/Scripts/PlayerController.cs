d9d3d1afb305dab5093238233c9973902448012c
quimHM <83378509+quimHM@users.noreply.github.com>
2021-04-29 01:53:41 +0200

    Add files via upload

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour {
+	
+	public float speed;
+	public Text countText;
+	public Text winText;
+
+	private Rigidbody rb;
+	private int count;
+
+	void Start ()
+	{
+		rb = GetComponent<Rigidbody>();
+
+		count = 0;
+
+		SetCountText ();
+		winText.text = "";
+	}
+
+	void FixedUpdate ()
+	{
+		float movementX = Input.GetAxis ("Horizontal");
+		float movementY = Input.GetAxis ("Vertical");
+		Vector3 movement = new Vector3 (movementX, 0.0f, movementY);
+		rb.AddForce (movement * speed);
+	}
+
+	void OnTriggerEnter(Collider other) 
+	{
+		if (other.gameObject.CompareTag ("Pick Up"))
+		{
+			other.gameObject.SetActive (false);
+			count += 1;
+			SetCountText();
+		}
+	}
+
+	void SetCountText()
+	{
+		countText.text = "Points Collected: " + count.ToString ();
+		if (count >= 4) 
+		{
+			winText.text = "Yey! You Won";
+		}
+	}
+}
---
+++ Assets/Scripts/Rotator.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	private float phase;
+	public bool moving;
+
+	void Start(){
+		phase = Mathf.Atan(transform.position.y/transform.position.x);
+		if (transform.position.x < 0){phase+=Mathf.PI;}
+	}
+
+	void Update () 
+	{
+		if (moving) transform.position = new Vector3 (8*Mathf.Cos(Time.time+phase),0.5f,8*Mathf.Sin(Time.time+phase));
+		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	}
+}	
